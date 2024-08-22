8d1b52bdfa8420cef629302996e6dc7187fe19a8
saah13 <32568433+saah13@users.noreply.github.com>
2024-08-22 15:09:45 -0600

    Create generate-pipeline.sh

---
+++ .buildkite/generate-pipeline.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Determine the value of MY_BOOLEAN_VAR
+if [ "$MY_BOOLEAN_VAR" == "true" ]; then
+  cat <<EOF
+steps:
+  - label: "Dynamic Sleep Test"
+    command: |
+      sleep 5
+    retry:
+      manual: true
+EOF
+else
+  cat <<EOF
+steps:
+  - label: "Dynamic Sleep Test (No Retry)"
+    command: |
+      sleep 5
+    retry:
+      manual: false
+EOF
+fi
