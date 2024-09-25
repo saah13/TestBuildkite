c727d9ea89f296e145768299e5c1efa239dcebfe
saah13 <32568433+saah13@users.noreply.github.com>
2024-09-24 18:49:26 -0600

    Update generate-pipeline.sh

--- .buildkite/generate-pipeline.sh
+++ .buildkite/generate-pipeline.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
-# Retrieve the deploy environment metadata
-ENVIRONMENTT=$(buildkite-agent meta-data get deploy-environment)
-echo "Selected Environment: $ENVIRONMENTT"
+ENVIRONMENT=$(buildkite-agent meta-data get deploy-environment)
+echo "$ENVIRONMENT"
 
-# Check which environment is selected and generate the corresponding pipeline steps
-if [ "$ENVIRONMENTT" == "sandbox" ]; then
-  cat <<EOF
-steps:
-  - trigger: "qa-playwright-tests-enterprise-portal-sandbox"
-    async: true
+if [[ "$ENVIRONMENT" == "sandbox" ]]; then
+  buildkite-agent pipeline upload <<EOF
+  steps:
+    - trigger: "qa-playwright-tests-enterprise-portal-sandbox"
+      async: true
 EOF
-elif [ "$ENVIRONMENTT" == "int" ]; then
-  cat <<EOF
-steps:
-  - trigger: "qa-playwright-tests-enterprise-portal-int"
-    async: true
+elif [[ "$ENVIRONMENT" == "int" ]]; then
+  buildkite-agent pipeline upload <<EOF
+  steps:
+    - trigger: "qa-playwright-tests-enterprise-portal-int"
+      async: true
 EOF
-else
-  echo "Unknown environment: $ENVIRONMENTT"
-  exit 1
 fi
+
