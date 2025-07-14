ae8df6ab3c2893523ecc2b9cc9500df9ea6d3aae
saah13 <32568433+saah13@users.noreply.github.com>
2025-07-14 17:22:46 -0600

    Update observed-step.ps1

--- .buildkite/scripts/observed-step.ps1
+++ .buildkite/scripts/observed-step.ps1
@@ -1,26 +1,25 @@
-# Setup
 $buildId = $Env:BUILDKITE_BUILD_ID
 $buildStart = [int](Get-Date -UFormat %s)
-$projectDir = "C:\buildkite-agent\builds\Agent1\larchi\starter"  # replace with your real path if needed
+$projectDir = "C:\buildkite-agent\builds\Agent1\larchi\starter"
 
-# Step 1: npm install
+# npm install
 $span1 = "install"
 $start1 = [int](Get-Date -UFormat %s)
 C:\buildevents\buildevents.exe cmd --shell powershell.exe $buildId $span1 install -- -Command "cd $projectDir; npm install"
 C:\buildevents\buildevents.exe step $buildId $span1 $start1 install
 
-# Step 2: npm run build
+# npm run build
 $span2 = "build"
 $start2 = [int](Get-Date -UFormat %s)
 C:\buildevents\buildevents.exe cmd --shell powershell.exe $buildId $span2 build -- -Command "cd $projectDir; npm run build"
 C:\buildevents\buildevents.exe step $buildId $span2 $start2 build
 
-# Step 3: npm test
+# npm test
 $span3 = "test"
 $start3 = [int](Get-Date -UFormat %s)
 C:\buildevents\buildevents.exe cmd --shell powershell.exe $buildId $span3 test -- -Command "cd $projectDir; npm test"
 C:\buildevents\buildevents.exe step $buildId $span3 $start3 test
 
-# End trace
+# Finish trace
 $traceUrl = C:\buildevents\buildevents.exe build $buildId $buildStart success
 Write-Host "Trace uploaded: $traceUrl"
