75ce625313a27a2af8ba9d08128db3e66a2b026d
saah13 <32568433+saah13@users.noreply.github.com>
2025-07-14 17:15:19 -0600

    Update observed-step.ps1

--- .buildkite/scripts/observed-step.ps1
+++ .buildkite/scripts/observed-step.ps1
@@ -2,12 +2,12 @@ $Env:BUILD_START = [int](Get-Date -UFormat %s)
 $Env:STEP_START = [int](Get-Date -UFormat %s)
 $Env:STEP_SPAN_ID = "windows-step"
 
-# Run the command using PowerShell shell properly
+# Run a traced command using PowerShell
 C:\buildevents\buildevents.exe cmd --shell powershell.exe $Env:BUILDKITE_BUILD_ID $Env:STEP_SPAN_ID hello -- -Command "echo 'Hello from Buildkite on Windows!'"
 
-# Close the step
+# End the step
 C:\buildevents\buildevents.exe step $Env:BUILDKITE_BUILD_ID $Env:STEP_SPAN_ID $Env:STEP_START windows-step
 
-# Complete the build and print trace link
+# Finish build and output Honeycomb trace
 $traceUrl = C:\buildevents\buildevents.exe build $Env:BUILDKITE_BUILD_ID $Env:BUILD_START success
 Write-Output "Honeycomb trace: $traceUrl"
