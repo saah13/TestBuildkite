626d26889d7e6e5d3671631d0f9ca2314dfb48b4
saah13 <32568433+saah13@users.noreply.github.com>
2025-07-23 18:35:39 -0600

    Update trace-install.ps1

--- .buildkite/scripts/trace-install.ps1
+++ .buildkite/scripts/trace-install.ps1
@@ -2,8 +2,12 @@ $env:BUILD_START = [int](Get-Date -UFormat %s)
 $env:STEP_START = [int](Get-Date -UFormat %s)
 $env:STEP_SPAN_ID = "npm-install"
 
-# Instrumented step
-C:\buildevents\buildevents.exe cmd $env:BUILDKITE_BUILD_ID $env:STEP_SPAN_ID npm-install -- cmd /c "cd C:\buildkite-agent\builds\Agent1\larchi\starter && npm install"
+# Run traced command
+C:\buildevents\buildevents.exe cmd `
+  $env:BUILDKITE_BUILD_ID `
+  $env:STEP_SPAN_ID `
+  npm-install `
+  -- cmd /c "cd C:\buildkite-agent\builds\Agent1\larchi\starter && npm install"
 
 # End span and build
 C:\buildevents\buildevents.exe step $env:BUILDKITE_BUILD_ID $env:STEP_SPAN_ID $env:STEP_START npm-install
